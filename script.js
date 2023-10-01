72dba02f1aa277cc12f560484aa3a15980dc89cd
Ben-Long50 <benjlong50@gmail.com>
2023-09-30 17:43:41 -0700

    added score keeping, invalid entry and retrying ties functionality to playGameRound function

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 game();
 function game(){
     let gameChoice = ['rock', 'paper', 'scissors'];
+    let computerRoundsWon = 0;
+    let playerRoundsWon = 0;
+
     for(let i = 1; i <= 5; i++){
 
-        alert(`Round ${i} go!`)
+        alert(`The score is currnetly Player: ${playerRoundsWon} Computer: ${computerRoundsWon} Round ${i} go!`)
+
         let playerChoice = prompt('Choose Rock, Paper or Scissors').toLowerCase();
         console.log(playerChoice);
 
@@ -19,28 +23,41 @@ function game(){
             }
 
         function playGameRound(playerSelection, computerSelection){
-            if(playerSelection === 'rock' && computerSelection === 'paper'){
+            if(playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors'){
+                i--;
+                return alert('Invalid input. Try again')
+            }
+            else if(playerSelection === 'rock' && computerSelection === 'paper'){
+                computerRoundsWon++;
                 return 'You Lose! Paper beats Rock'
             }
             else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+                computerRoundsWon++;
                 return 'You Lose! Scissors beats Paper'
             }
             else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+                computerRoundsWon++;
                 return 'You Lose! Rock beats Scissors'
             }
             else if(playerSelection === 'rock' && computerSelection === 'rock'){
-                return 'It\'s a tie!'
+                i--;
+                return 'It\'s a tie! Try again'
             }
             else if(playerSelection === 'paper' && computerSelection === 'paper'){
-                return 'It\'s a tie!'
+                i--;
+                return 'It\'s a tie! Try again'
             }
             else if(playerSelection === 'scissors' && computerSelection === 'scissors'){
-                return 'It\'s a tie!'
+                i--;
+                return 'It\'s a tie! Try again'
             }
             else{
+                playerRoundsWon++;
                 return 'You win!'
+                
             }
         }
 
     }
+    return alert(`The final score is Player: ${playerRoundsWon} Computer: ${computerRoundsWon}`)
 }
