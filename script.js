f8869b083153233b1e1d5bcefc08dd3597b66929
Ben-Long50 <benjlong50@gmail.com>
2023-09-30 17:09:21 -0700

    added playGameRound function in javascript

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-gameChoice = ['Rock', 'Paper', 'Scissors'];
 
-function getComputerChoice(computerChoice){
-    return computerChoice[Math.floor((Math.random()*computerChoice.length))];
+game();
+function game(){
+    let gameChoice = ['rock', 'paper', 'scissors'];
+    for(let i = 1; i <= 5; i++){
+
+        alert(`Round ${i} go!`)
+        let playerChoice = prompt('Choose Rock, Paper or Scissors').toLowerCase();
+        console.log(playerChoice);
+
+        let computerChoice = getComputerChoice(gameChoice);
+        console.log(computerChoice);
+
+        let gameOutcome = playGameRound(playerChoice, computerChoice);
+        console.log(gameOutcome);
+
+        function getComputerChoice(gameOptions){
+            return gameOptions[Math.floor((Math.random()*gameOptions.length))];
+            }
+
+        function playGameRound(playerSelection, computerSelection){
+            if(playerSelection === 'rock' && computerSelection === 'paper'){
+                return 'You Lose! Paper beats Rock'
+            }
+            else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+                return 'You Lose! Scissors beats Paper'
+            }
+            else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+                return 'You Lose! Rock beats Scissors'
+            }
+            else if(playerSelection === 'rock' && computerSelection === 'rock'){
+                return 'It\'s a tie!'
+            }
+            else if(playerSelection === 'paper' && computerSelection === 'paper'){
+                return 'It\'s a tie!'
+            }
+            else if(playerSelection === 'scissors' && computerSelection === 'scissors'){
+                return 'It\'s a tie!'
+            }
+            else{
+                return 'You win!'
+            }
+        }
+
     }
-
-console.log(getComputerChoice(gameChoice));
+}
