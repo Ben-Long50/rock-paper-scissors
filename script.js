865ea985511accc3656072fe0be3ddf146b23549
Ben-Long50 <benjlong50@gmail.com>
2023-10-12 23:20:55 -0700

    added live scorekeeping functionality and end of game condition functions

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-
 const gameChoice = ['sword', 'shield', 'bow'];
 const computerRoundsWon = 0;
 const playerRoundsWon = 0;
+let currentChampionScore = 0;
+let currentAdversaryScore = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
+    
     const swordButton = document.querySelector('.sword');
     const shieldButton = document.querySelector('.shield');
     const bowButton = document.querySelector('.bow');
+    const championScore = document.querySelector('.champion-score');
+    const adversaryScore = document.querySelector('.adversary-score');
+ 
+    championScore.textContent = currentChampionScore;
+    adversaryScore.textContent = currentAdversaryScore;
 
     function buttonColorChange(button, color1, color2){
         button.style.backgroundColor = color1;
         button.style.borderColor = color2;
+    }
+
+    function updateScores(){
+        championScore.textContent = currentChampionScore;
+        adversaryScore.textContent = currentAdversaryScore;
+    }
+    
+    function gameEndCondition(){
+        if(currentAdversaryScore === 5){
+            alert('You\'ve been bested in combat. Try again! Victory is but a few clicks away!');
+            currentAdversaryScore = 0;
+            currentChampionScore = 0;
+            updateScores();
+        }
+        else if(currentChampionScore === 5){
+            alert('You are victorious in battle! On to the next adversary.');
+            currentAdversaryScore = 0;
+            currentChampionScore = 0;
+            updateScores();
+        }
     }
 
     swordButton.addEventListener('mouseover', function() {
@@ -36,62 +63,61 @@ document.addEventListener('DOMContentLoaded', function() {
     bowButton.addEventListener('mouseout', function() {
         buttonColorChange(bowButton, 'rgb(175, 175, 175)', 'rgb(0, 134, 40)');
     });
+
+    swordButton.addEventListener('click', function(){
+        const computerChoice = getComputerChoice(gameChoice);
+        playerChoice = 'sword';
+        playGameRound(playerChoice, computerChoice);
+        gameEndCondition();
+    })
+
+    shieldButton.addEventListener('click', function(){
+        const computerChoice = getComputerChoice(gameChoice);
+        playerChoice = 'shield';
+        playGameRound(playerChoice, computerChoice)
+        gameEndCondition();
+    })
+
+    bowButton.addEventListener('click', function(){
+        const computerChoice = getComputerChoice(gameChoice);
+        playerChoice = 'bow';
+        playGameRound(playerChoice, computerChoice);
+        gameEndCondition();
+    })
+
+    function getComputerChoice(gameOptions){
+        return gameOptions[Math.floor((Math.random()*gameOptions.length))];
+    }
+
+    function playGameRound(playerSelection, computerSelection){
+        if(playerSelection === 'sword' && computerSelection === 'shield'){
+            currentAdversaryScore++;
+            updateScores();
+            return alert('You Lose! Your attack was deflected')
+        }
+        else if(playerSelection === 'shield' && computerSelection === 'bow'){
+            currentAdversaryScore++;
+            updateScores();
+            return alert('You Lose! You\'ve been shot in the leg')
+        }
+        else if(playerSelection === 'bow' && computerSelection === 'sword'){
+            currentAdversaryScore++;
+            updateScores();
+            return alert('You Lose! You\'ve been stabbed in the back')
+        }
+        else if(playerSelection === 'sword' && computerSelection === 'sword'){
+            return alert('It\'s a tie! Try again')
+        }
+        else if(playerSelection === 'shield' && computerSelection === 'shield'){
+            return alert('It\'s a tie! Try again')
+        }
+        else if(playerSelection === 'bow' && computerSelection === 'bow'){
+            return alert('It\'s a tie! Try again')
+        }
+        else{
+            currentChampionScore++;
+            updateScores();
+            return alert('You win!')
+        }
+    }
 });
-// function game(){
-
-//     for(let i = 1; i <= 5; i++){
-
-//         alert(`The score is currnetly Player: ${playerRoundsWon} Computer: ${computerRoundsWon} Round ${i} go!`)
-
-//         const playerChoice = prompt('Choose , Paper or Scissors').toLowerCase();
-//         console.log(playerChoice);
-
-//         let computerChoice = getComputerChoice(gameChoice);
-//         console.log(computerChoice);
-
-//         let gameOutcome = playGameRound(playerChoice, computerChoice);
-//         console.log(gameOutcome);
-
-//         function getComputerChoice(gameOptions){
-//             return gameOptions[Math.floor((Math.random()*gameOptions.length))];
-//             }
-
-//         function playGameRound(playerSelection, computerSelection){
-//             if(playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors'){
-//                 i--;
-//                 return alert('Invalid input. Try again')
-//             }
-//             else if(playerSelection === 'rock' && computerSelection === 'paper'){
-//                 computerRoundsWon++;
-//                 return alert('You Lose! Paper beats Rock')
-//             }
-//             else if(playerSelection === 'paper' && computerSelection === 'scissors'){
-//                 computerRoundsWon++;
-//                 return alert('You Lose! Scissors beats Paper')
-//             }
-//             else if(playerSelection === 'scissors' && computerSelection === 'rock'){
-//                 computerRoundsWon++;
-//                 return alert('You Lose! Rock beats Scissors')
-//             }
-//             else if(playerSelection === 'rock' && computerSelection === 'rock'){
-//                 i--;
-//                 return alert('It\'s a tie! Try again')
-//             }
-//             else if(playerSelection === 'paper' && computerSelection === 'paper'){
-//                 i--;
-//                 return alert('It\'s a tie! Try again')
-//             }
-//             else if(playerSelection === 'scissors' && computerSelection === 'scissors'){
-//                 i--;
-//                 return alert('It\'s a tie! Try again')
-//             }
-//             else{
-//                 playerRoundsWon++;
-//                 return alert('You win!')
-                
-//             }
-//         }
-
-//     }
-//     return alert(`The final score is Player: ${playerRoundsWon} Computer: ${computerRoundsWon}`)
-// }
